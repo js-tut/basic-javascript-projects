d1472e2487c226575558a94f122faba617cbd3da
john-smilga <johnsmilga@gmail.com>
2020-04-28 15:35:00 -0700

    course link added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,16 +20,26 @@ const query = graphql`
         }
       }
     }
+    course: contentfulCourses(title: { eq: "javascript" }) {
+      title
+      url
+    }
   }
 `
 
 const Projects = () => {
   const {
     jsProjects: { nodes: projects },
+    course: { url },
   } = useStaticQuery(query)
 
   return (
     <section className="section">
+      <div className="course-link">
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          start course
+        </a>
+      </div>
       <div className="section-title">
         <h2>all projects</h2>
         <div className="underline"></div>
